050b9bd28832cb4b24836766b38f867d13aaefb3
YosukeKono0101 <n11230380@qut.edu.au>
2024-03-30 19:57:24 +1000

    Slight Design Change

--- src/components/LoadingSpinner.js
+++ src/components/LoadingSpinner.js
@@ -3,8 +3,15 @@ import { BeatLoader } from "react-spinners";
 
 const LoadingSpinner = () => {
   return (
-    <div style={{ display: "flex", justifyContent: "center", marginTop: "50px" }}>
-      <BeatLoader color="black" />
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "50vh",
+      }}
+    >
+      <BeatLoader color="white" />
     </div>
   );
 };
