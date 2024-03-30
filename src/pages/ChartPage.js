e9d6e473b9711caa06435512aba937c8c6ccabd5
YosukeKono0101 <n11230380@qut.edu.au>
2024-03-30 20:07:46 +1000

    Created ChartPage

---
+++ src/pages/ChartPage.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import styled, { createGlobalStyle } from "styled-components";
+import { fetchTopTracks, fetchTopAlbums } from "../services/lastfmAPI";
+import TopTracksChart from "../components/TopTracksChart";
+import AlbumsChart from "../components/AlbumsChart";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    background-color: black;
+    color: white;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+  }
+`;
+
+const ChartContainer = styled.div`
+  max-width: 1200px;
+  margin: 20px auto;
+  padding: 20px;
+`;
+
+const Title = styled.h1`
+  text-align: center;
+  color: #fff;
+  margin: 40px 0;
+`;
+
+const ChartPage = () => {
+  const [topTracks, setTopTracks] = useState([]);
+  const [topAlbums, setTopAlbums] = useState([]);
+
+  useEffect(() => {
+    const loadData = async () => {
+      const tracks = await fetchTopTracks();
+      const albums = await fetchTopAlbums();
+      setTopTracks(tracks);
+      setTopAlbums(albums);
+    };
+
+    loadData();
+  }, []);
+
+  return (
+    <>
+      <GlobalStyle />
+      <ChartContainer>
+        <Title>Top Tracks</Title>
+        <TopTracksChart tracks={topTracks} />
+        <Title>Top Albums</Title>
+        <AlbumsChart albums={topAlbums} />
+      </ChartContainer>
+    </>
+  );
+};
+
+export default ChartPage;
