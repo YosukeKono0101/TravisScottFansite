27a6eea77796de401ce59707173ff61ce89d0348
YosukeKono0101 <n11230380@qut.edu.au>
2024-03-30 19:56:43 +1000

    Responsive Design Added

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import heroImg from "../images/hero_image.webp";
+import heroImg from "../images/hero_image2.jpeg";
 
 const HeroContainer = styled.div`
   text-align: center;
@@ -14,18 +14,45 @@ const HeroContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  @media (max-width: 768px) {
+    padding: 100px 20px;
+    height: auto;
+  }
+
+  @media (min-width: 769px) and (max-width: 1024px) {
+    padding: 150px 20px;
+    height: auto;
+  }
 `;
 
 const Title = styled.h1`
   font-size: 48px;
   font-weight: bold;
   margin-bottom: 20px;
+
+  @media (max-width: 768px) {
+    font-size: 32px;
+    padding-top: 70px;
+  }
+
+  @media (min-width: 769px) and (max-width: 1024px) {
+    font-size: 40px;
+  }
 `;
 
 const Subtext = styled.div`
   margin-bottom: 30px;
   color: #fff;
   font-size: 24px;
+
+  @media (max-width: 768px) {
+    font-size: 18px;
+  }
+
+  @media (min-width: 769px) and (max-width: 1024px) {
+    font-size: 20px;
+  }
 `;
 
 const HeroSection = () => (
