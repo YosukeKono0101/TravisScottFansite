b060c2f6c17a4c2c322f7cdd46200bf31ebd8454
YosukeKono0101 <n11230380@qut.edu.au>
2024-03-30 19:52:30 +1000

    Chart Route Added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import styled from "styled-components";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -8,20 +9,36 @@ import Resume from "./pages/Resume";
 import Portfolio from "./pages/Portfolio";
 import VideoDetail from "./pages/VideoDetailPage";
 import AlbumDetail from "./pages/AlbumDetailPage";
+import ChartPage from "./pages/ChartPage";
+
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
+const Content = styled.main`
+  flex: 1;
+`;
 
 function App() {
   return (
     <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/resume" element={<Resume />} />
-        <Route path="/portfolio" element={<Portfolio />} />
-        <Route path="/video/:videoId" element={<VideoDetail />} />
-        <Route path="/album/:artistName/:albumName" element={<AlbumDetail />} />
-      </Routes>
-      <Footer />
+      <AppContainer>
+        <Content>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/resume" element={<Resume />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/chart" element={<ChartPage />} />
+            <Route path="/video/:videoId" element={<VideoDetail />} />
+            <Route path="/album/:artistName/:albumName" element={<AlbumDetail />} />
+          </Routes>
+        </Content>
+        <Footer />
+      </AppContainer>
     </Router>
   );
 }
